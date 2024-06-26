14447a11945e7e027c901903bb3ad18464caf58e
ramin98 <59912261+ramin98@users.noreply.github.com>
2024-06-26 10:19:39 +0400

    Add files via upload

---
+++ react/reducer and context/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import BagGoods from "./BagGoods"
+
+function Header() {
+
+    return (
+      <header>
+        <h1>HEADER</h1>
+        <BagGoods/>
+      </header>
+    )
+  }
+  
+  export default Header
---
+++ react/reducer and context/src/components/MainComp.jsx
@@ -0,0 +1,12 @@
+import GoodsList from "./GoodsList"
+
+function MainComp() {
+
+    return (
+      <main>
+        <GoodsList/>
+      </main>
+    )
+  }
+  
+  export default MainComp
